a3d22ab14fd03dc0bc488968a67a263e98758db6
tofu0924 <in_no@naver.com>
2022-11-10 23:07:47 +0900

    :2nd commit

---
+++ src/main/java/scone/sconespring/SconeSpringApplication.java
@@ -0,0 +1,13 @@
+package scone.sconespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SconeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SconeSpringApplication.class, args);
+	}
+
+}
